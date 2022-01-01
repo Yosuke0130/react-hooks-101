f9f90d9ab7483bd1fd31fa0a0a47bad6eaa6a63a
yosuke.koma <yosuke.5pit2@gmail.com>
2022-01-01 16:19:51 +0900

    implement all events deletion feature

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,12 +1,13 @@
-import React from 'core-js/library/fn/reflect/es7/metadata'
+import React from 'react'
 const Event = ({ dispatch, event }) => {
     const id = event.id
     const handleClickDeleteButton = () => {
-      dispatch({ type: 'DELETE_EVENT', id })
+        const result = window.confirm(`このイベント(id=${id})を本当に削除しますか？`)
+        if(result) dispatch({ type: 'DELETE_EVENT', id })
     }
     return (
       <tr>
-        <td>{event.id}</td>
+        <td>{id}</td>
         <td>{event.title}</td>
         <td>{event.body}</td>
         <td><button type="button" className="btn btn-danger" onClick={handleClickDeleteButton}>削除</button></td>
